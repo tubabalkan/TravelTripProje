3c04f23a530a2f51a36f86d1875d0593a0cbdb0c
tuba balkan <tubabalkan165@gmail.com>
2024-02-26 13:05:53 +0300

    Add project files.

---
+++ TravelTripProje/Models/Sınıflar/Yorum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class Yorum
+    {
+        [Key]
+        public int Id { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Mail { get; set; }
+        public string yorum { get; set; }
+        public int Blogid { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+}
