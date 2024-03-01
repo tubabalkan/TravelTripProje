f268e48d2635500f63617295823af79b927d2361
tuba balkan <tubabalkan165@gmail.com>
2024-03-01 16:29:24 +0300

    ekleme yapıldı

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using TravelTripProje.Models.Sınıflar;
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            
+            var degerler=c.Blogs.Take(4).ToList();
+            return View(degerler);
         }
         public ActionResult About()
         {
-            
             return View();
+        }
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x => x.ID).ToList();
+            return PartialView(deger);
         }
 
     }
